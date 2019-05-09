67847e1cf35a612235781636b80113cfece32a51
ropeks <ropeksmilioner@gmail.com>
2019-05-09 14:54:51 +0200

    create person, instructor and student classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,55 @@
 // CODE here for your Lambda Classes
+
+class Person {
+    constructor(name, age, location, gender) {
+        this.name = name;
+        this.age = age;
+        this.location = location;
+        this.gender = gender;
+    }
+
+    speak() {
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
+    }
+}
+
+class Instructor extends Person {
+    constructor(name, age, location, gender, specialty, favLanguage, catchPhrase) {
+        super(name, age, location, gender);
+        this.specialty = specialty;
+        this.favLanguage = favLanguage;
+        this.catchPhrase = catchPhrase;
+    }
+
+    demo(subject) {
+        console.log(`Today we are learning about ${subject}`);
+    }
+
+    grade(student, subject) {
+        console.log(`${student.name} receives a perfect score on ${subject}.`);
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, location, gender, previousBackground, className, favSubjects) {
+        super(name, age, location, gender);
+        this.previousBackground = previousBackground;
+        this.className = className;
+        this.favSubjects = favSubjects;
+    }
+
+    listsSubjects () {
+        for (let i = 0; i < this.favSubjects.length; i++) {
+            console.log(this.favSubjects[i]);
+        }
+    }
+
+    PRAssignment (subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}.`);
+    }
+
+    sprintChallenge (subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}.`);
+    }
+}
+
